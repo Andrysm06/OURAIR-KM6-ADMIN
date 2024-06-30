7f2830504b1e2d80ace1f66c43539dce06dd1c1c
Andrysm06 <andrysetiawanm2206@gmail.com>
2024-06-30 13:46:18 +0700

    update tableUser

--- src/Page/AdminUserTable.jsx
+++ src/Page/AdminUserTable.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../Layout/Navbar";
+import Footer from "../Layout/Footer";
 
 const AdminUserTable = () => {
   const [users, setUsers] = useState([
@@ -35,7 +36,9 @@ const AdminUserTable = () => {
   });
 
   const [isEditing, setIsEditing] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false); // State untuk mengatur modal
+  const [modalOpen, setModalOpen] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [userToDelete, setUserToDelete] = useState(null);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -45,19 +48,31 @@ const AdminUserTable = () => {
   const handleAddUser = () => {
     setUsers([...users, { ...formData, id: users.length + 1 }]);
     setFormData({ id: null, name: "", email: "", role: "", status: "Active" });
+    setModalOpen(false);
   };
 
   const handleEditUser = (user) => {
     setIsEditing(true);
     setFormData(user);
-    setModalOpen(true); // Buka modal saat mengklik tombol Edit
+    setModalOpen(true);
   };
 
   const handleUpdateUser = () => {
     setUsers(users.map((user) => (user.id === formData.id ? formData : user)));
     setIsEditing(false);
     setFormData({ id: null, name: "", email: "", role: "", status: "Active" });
-    setModalOpen(false); // Tutup modal setelah update user
+    setModalOpen(false);
+  };
+
+  const handleDeleteUser = (userId) => {
+    setDeleteModalOpen(true);
+    setUserToDelete(userId);
+  };
+
+  const confirmDeleteUser = () => {
+    setUsers(users.filter((user) => user.id !== userToDelete));
+    setDeleteModalOpen(false);
+    setUserToDelete(null);
   };
 
   const closeModal = () => {
@@ -66,13 +81,19 @@ const AdminUserTable = () => {
     setFormData({ id: null, name: "", email: "", role: "", status: "Active" });
   };
 
+  const closeDeleteModal = () => {
+    setDeleteModalOpen(false);
+    setUserToDelete(null);
+  };
+
   return (
     <div>
       <Navbar />
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto mt-8 md:mt-24 space-y-12 h-screen px-4">
         <h1 className="text-2xl font-bold mb-4">User Management</h1>
+
         <div className="overflow-x-auto mb-4">
-          <table className="min-w-full bg-white border border-gray-200">
+          <table className="min-w-full bg-white border border-gray-200 rounded-lg">
             <thead>
               <tr>
                 <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
@@ -128,13 +149,24 @@ const AdminUserTable = () => {
                     >
                       Edit
                     </button>
+                    <button
+                      className="text-red-600 hover:text-red-900"
+                      onClick={() => handleDeleteUser(user.id)}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
-        {/* Modal */}
+        <button
+          className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setModalOpen(true)}
+        >
+          Add User
+        </button>
         {modalOpen && (
           <div className="fixed inset-0 z-10 overflow-y-auto">
             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -148,7 +180,7 @@ const AdminUserTable = () => {
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                       <h3 className="text-lg leading-6 font-medium text-gray-900">
-                        Edit User
+                        {isEditing ? "Edit User" : "Add User"}
                       </h3>
                       <div className="mt-2">
                         <form className="w-full max-w-lg">
@@ -214,27 +246,16 @@ const AdminUserTable = () => {
                               >
                                 Status
                               </label>
-                              <div className="relative">
-                                <select
-                                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                  id="status"
-                                  name="status"
-                                  value={formData.status}
-                                  onChange={handleInputChange}
-                                >
-                                  <option value="Active">Active</option>
-                                  <option value="Inactive">Inactive</option>
-                                </select>
-                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                  <svg
-                                    className="fill-current h-4 w-4"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                  >
-                                    <path d="M7 10l5 5 5-5H7z" />
-                                  </svg>
-                                </div>
-                              </div>
+                              <select
+                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="status"
+                                name="status"
+                                value={formData.status}
+                                onChange={handleInputChange}
+                              >
+                                <option value="Active">Active</option>
+                                <option value="Inactive">Inactive</option>
+                              </select>
                             </div>
                           </div>
                         </form>
@@ -243,15 +264,27 @@ const AdminUserTable = () => {
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  {isEditing ? (
+                    <button
+                      type="button"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={handleUpdateUser}
+                    >
+                      Update
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={handleAddUser}
+                    >
+                      Add
+                    </button>
+                  )}
                   <button
-                    onClick={handleUpdateUser}
-                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  >
-                    Update User
-                  </button>
-                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={closeModal}
-                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                   >
                     Cancel
                   </button>
@@ -260,28 +293,50 @@ const AdminUserTable = () => {
             </div>
           </div>
         )}
-        {/* End Modal */}
-        <form className="w-full max-w-lg">
-          <div className="flex items-center justify-between">
-            {!isEditing ? (
-              <button
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-                onClick={handleAddUser}
-              >
-                Add User
-              </button>
-            ) : (
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-                onClick={handleUpdateUser}
-              >
-                Update User
-              </button>
-            )}
+        {deleteModalOpen && (
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div className="fixed inset-0 transition-opacity">
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+              </div>
+              <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+              &#8203;
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <h3 className="text-lg leading-6 font-medium text-gray-900">
+                        Konfirmasi penghapusan
+                      </h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Apakah Anda yakin ingin menghapus pengguna ini? Aksi
+                          ini tidak dapat dibatalkan.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={confirmDeleteUser}
+                  >
+                    Hapus
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={closeDeleteModal}
+                  >
+                    Batal
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-        </form>
+        )}
       </div>
     </div>
   );
